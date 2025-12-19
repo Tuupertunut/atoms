00af503d4e18931b1b3b2e9e96acb1cee1a85cc3
Tuupertunut <tuupertunut@outlook.com>
2025-12-19 03:15:13 +0200

    Add lammps hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::ptr;
+use std::{ffi::CString, ptr};
 
 mod lammps;
 
@@ -6,6 +6,10 @@ fn main() {
     println!("Hello, world!");
     unsafe {
         let simulation = lammps::lammps_open_no_mpi(0, ptr::null_mut(), ptr::null_mut());
+        lammps::lammps_command(
+            simulation,
+            CString::new("print 'lammps hello world'").unwrap().as_ptr(),
+        );
         lammps::lammps_close(simulation);
     }
 }
