9bc0adedd7c861c35af0ba9bfb36080f61a862e3
Tuupertunut <tuupertunut@outlook.com>
2025-12-21 22:39:28 +0200

    Create semi-safe wrapper around lammps

--- src/lammps.rs
+++ src/lammps.rs
@@ -1,2 +1,44 @@
-#![allow(nonstandard_style, dead_code)]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+use lammps_sys::*;
+use std::{
+    ffi::{CString, c_void},
+    ptr,
+};
+
+/// Lammps C API bindings generated from library.h
+mod lammps_sys {
+    #![allow(nonstandard_style, dead_code)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+/// A semi-safe wrapper for lammps C API
+pub struct Lammps {
+    pub session: *mut c_void,
+}
+
+impl Lammps {
+    pub fn open() -> Self {
+        return Self {
+            session: unsafe { lammps_open_no_mpi(0, ptr::null_mut(), ptr::null_mut()) },
+        };
+    }
+
+    pub fn command(&mut self, command: &str) {
+        unsafe { lammps_command(self.session, CString::new(command).unwrap().as_ptr()) };
+    }
+
+    pub fn get_natoms(&self) -> usize {
+        return unsafe { lammps_get_natoms(self.session) } as usize;
+    }
+
+    pub fn extract_atom(&self, property: &str) -> *mut c_void {
+        return unsafe {
+            lammps_extract_atom(self.session, CString::new(property).unwrap().as_ptr())
+        };
+    }
+}
+
+impl Drop for Lammps {
+    fn drop(&mut self) {
+        unsafe { lammps_close(self.session) };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,34 @@
-use std::{ffi::CString, ptr, slice};
+use lammps::Lammps;
+use std::slice;
 
 mod lammps;
 
 fn main() {
     println!("Hello, world!");
+    let mut simulation = Lammps::open();
+
+    simulation.command("units metal");
+    simulation.command("dimension 3");
+    simulation.command("boundary p p p");
+    simulation.command("atom_style atomic");
+    simulation.command("region box block 0 20 0 20 0 20");
+    simulation.command("create_box 1 box");
+    simulation.command("create_atoms 1 single 10 10 10");
+    simulation.command("create_atoms 1 single 15 10 10");
+    simulation.command("mass 1 40");
+    simulation.command("pair_style lj/cut 7");
+    simulation.command("pair_coeff 1 1 0.01 3.3");
+    simulation.command("fix 1 all nve");
+    simulation.command("timestep 0.001");
+    simulation.command("run 1000");
+
+    let n_atoms = simulation.get_natoms();
+    println!("{}", n_atoms);
+
+    let coords = simulation.extract_atom("x") as *const *const [f64; 3];
     unsafe {
-        let simulation = lammps::lammps_open_no_mpi(0, ptr::null_mut(), ptr::null_mut());
-
-        lammps::lammps_command(simulation, CString::new("units metal").unwrap().as_ptr());
-        lammps::lammps_command(simulation, CString::new("dimension 3").unwrap().as_ptr());
-        lammps::lammps_command(simulation, CString::new("boundary p p p").unwrap().as_ptr());
-        lammps::lammps_command(
-            simulation,
-            CString::new("atom_style atomic").unwrap().as_ptr(),
-        );
-        lammps::lammps_command(
-            simulation,
-            CString::new("region box block 0 20 0 20 0 20")
-                .unwrap()
-                .as_ptr(),
-        );
-        lammps::lammps_command(
-            simulation,
-            CString::new("create_box 1 box").unwrap().as_ptr(),
-        );
-        lammps::lammps_command(
-            simulation,
-            CString::new("create_atoms 1 single 10 10 10")
-                .unwrap()
-                .as_ptr(),
-        );
-        lammps::lammps_command(
-            simulation,
-            CString::new("create_atoms 1 single 15 10 10")
-                .unwrap()
-                .as_ptr(),
-        );
-        lammps::lammps_command(simulation, CString::new("mass 1 40").unwrap().as_ptr());
-        lammps::lammps_command(
-            simulation,
-            CString::new("pair_style lj/cut 7").unwrap().as_ptr(),
-        );
-        lammps::lammps_command(
-            simulation,
-            CString::new("pair_coeff 1 1 0.01 3.3").unwrap().as_ptr(),
-        );
-        lammps::lammps_command(simulation, CString::new("fix 1 all nve").unwrap().as_ptr());
-        lammps::lammps_command(simulation, CString::new("timestep 0.001").unwrap().as_ptr());
-        lammps::lammps_command(simulation, CString::new("run 1000").unwrap().as_ptr());
-
-        let n_atoms = lammps::lammps_get_natoms(simulation) as usize;
-        println!("{}", n_atoms);
-
-        let coords = lammps::lammps_extract_atom(simulation, CString::new("x").unwrap().as_ptr())
-            as *const *const [f64; 3];
         for &atom_coords in slice::from_raw_parts(coords, n_atoms).into_iter() {
             println!("{:?}", *atom_coords);
         }
-
-        lammps::lammps_close(simulation);
     }
 }
